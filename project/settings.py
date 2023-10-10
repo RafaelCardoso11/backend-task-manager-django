4ca55e4e54839cf659826efb2a6fb5afa986fb7b
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-10-10 17:19:36 -0300

    feat: mysql container + connection

--- project/settings.py
+++ project/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('MYSQL_DATABASE'),
+        'USER': config('MYSQL_USER'),
+        'PASSWORD': config('MYSQL_PASSWORD'),
+        'HOST': '127.0.0.1',
+        'PORT': '3307',
     }
 }
 
